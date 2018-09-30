826a2c3ebc73cc8ee2bdaea7a876c0717db07eb4
Sydney Grant <sg3957@nyu.edu>
2018-09-30 00:56:29 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(arr)
   elsif arr.size == 3 
     return "#{arr[0]}, #{arr[1]}, and #{arr[2]}"
   elsif arr.size > 3 
-   new_str = " "
+   new_str = ""
    count = 0 
    while count < arr.size-1
     new_str << "#{arr[count]}, "
