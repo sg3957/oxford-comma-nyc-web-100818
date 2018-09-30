2efcbc8f022443057ccb3e64738f1f1152d7785d
Sydney Grant <sg3957@nyu.edu>
2018-09-30 00:52:04 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,15 @@ def oxford_comma(arr)
     return "#{arr[0]} and #{arr[1]}"
   elsif arr.size == 3 
     return "#{arr[0]}, #{arr[1]}, and #{arr[2]}"
+  elsif arr.size > 3 
+   new_arr = []
+   count = 0 
+   until count == arr.size
+    new_arr << arr[count]
+    count += 1 
+  end
+  new_arr << "and #{arr[count.last]}"
   arr.join(",")
+end
+  
 end
