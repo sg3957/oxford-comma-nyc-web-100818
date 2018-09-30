1a286c64861cc706200bcd691be5f8e538fd572a
Sydney Grant <sg3957@nyu.edu>
2018-09-30 00:52:43 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,7 @@
 def oxford_comma(arr)
-  if arr.size == 2 
+  if arr.size == 1 
+    return "#{arr[0]}"
+  elsif arr.size == 2 
     return "#{arr[0]} and #{arr[1]}"
   elsif arr.size == 3 
     return "#{arr[0]}, #{arr[1]}, and #{arr[2]}"
